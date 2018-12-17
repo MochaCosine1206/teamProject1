a67a5fcfed59a31da03e2936ae21962273ab975d
StevenXPS13 <steven.parrott@gmail.com>
2018-12-17 13:23:27 -0700

    datamuse working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,7 +5,54 @@ var config = {
     projectId: "teamproject1",
     storageBucket: "teamproject1.appspot.com",
     messagingSenderId: "1068555305070"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
+var search;
+var dataMuseQuery;
+console.log(search);
+
+
+
+$("#submitButton").on("click", function(event) {
+
+    event.preventDefault();
+
+    var textValue = $("#wordSearch").val().trim();
+    search = textValue.split(" ").join("+");
+    console.log(search);
+    
+    
+    console.log(search);
+    $("#submitButton").off("click")
+
+
+
+
+    dataMuseQuery = "https://cors-escape.herokuapp.com/https://api.datamuse.com/words?ml=" + search;
+
+    console.log(dataMuseQuery);
+
+$.ajax({
+    url: dataMuseQuery,
+    method: "GET",
+    success: function (response) {
+        console.log(response[0]);
+        console.log(response.length);
+        console.log(response[0].word);
+
+        for (var i = 0; i < response.length; i++) {
+            resultsLine = $("<li>")
+            console.log(resultsLine);
+            resultsLink = $("<a>").text(response[i].word);
+            console.log(resultsLink);
+            resultsLine.append(resultsLink);
+            $("#results").append(resultsLine);
+        }
+    }
+
+  });
+
+})
+
