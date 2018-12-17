b5e8894a74f59889bde857372ca8de49e58a8b17
StevenXPS13 <steven.parrott@gmail.com>
2018-12-17 07:23:16 -0700

    file setup, scripts added

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyB2Kcu48M0PHgRM5XAlmLM8slqwc08DirY",
+    authDomain: "teamproject1.firebaseapp.com",
+    databaseURL: "https://teamproject1.firebaseio.com",
+    projectId: "teamproject1",
+    storageBucket: "teamproject1.appspot.com",
+    messagingSenderId: "1068555305070"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
